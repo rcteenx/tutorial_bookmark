53414ea6125328b199307a23a7b85271bfdb370c
“rcteenx” <rcteenx@gmail.com>
2023-10-24 15:47:17 +0300

    finish website

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
-// Removes active classes for all tabs.
+// Hamburger button listener
+btn.addEventListener("click", navToggle);
+
 function onTabClick(e) {
+  // Deactivate all tabs
   tabs.forEach((tab) => {
     tab.children[0].classList.remove(
       "border-softRed",
@@ -14,15 +20,26 @@ function onTabClick(e) {
     );
   });
 
-  // Hide All Panels
+  // Hide all panels
   panels.forEach((panel) => panel.classList.add("hidden"));
 
-  // Active a new tab
+  // Activate a new tab and panel based on the target
   e.target.classList.add("border-softRed", "border-b-4");
-
   const classString = e.target.getAttribute("data-target");
   document
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
